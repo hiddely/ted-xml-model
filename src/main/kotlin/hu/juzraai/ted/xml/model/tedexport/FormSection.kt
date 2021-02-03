f4a7dbdea8b178f3f1819036380b2a7dff6d1e7b
Hidde L <mail.hidde@gmail.com>
2021-02-03 10:11:32 +0100

    Add F01 form

--- src/main/kotlin/hu/juzraai/ted/xml/model/tedexport/FormSection.kt
+++ src/main/kotlin/hu/juzraai/ted/xml/model/tedexport/FormSection.kt
@@ -1,5 +1,6 @@
 package hu.juzraai.ted.xml.model.tedexport
 
+import hu.juzraai.ted.xml.model.tedexport.form.F01_2014
 import hu.juzraai.ted.xml.model.tedexport.form.F02_2014
 import org.simpleframework.xml.ElementList
 import org.simpleframework.xml.Root
@@ -10,7 +11,11 @@ import org.simpleframework.xml.Root
 @Root(name = "FORM_SECTION", strict = false) // TODO
 data class FormSection (
 
-    @field:ElementList(inline = true, entry = "F02_2014", required = false)
+
+        @field:ElementList(inline = true, entry = "F01_2014", required = false)
+    var priorNotices: List<F01_2014> = mutableListOf<F01_2014>(),
+
+        @field:ElementList(inline = true, entry = "F02_2014", required = false)
     var contractNotices: List<F02_2014> = mutableListOf<F02_2014>()
 
 )
---
+++ src/main/kotlin/hu/juzraai/ted/xml/model/tedexport/form/F01_2014.kt
@@ -0,0 +1,28 @@
+package hu.juzraai.ted.xml.model.tedexport.form
+
+import hu.juzraai.ted.xml.model.meta.Compatible
+import hu.juzraai.ted.xml.model.meta.TedXmlSchemaVersion
+import hu.juzraai.ted.xml.model.tedexport.common.ContactContractingBody
+import org.simpleframework.xml.Attribute
+import org.simpleframework.xml.Element
+import org.simpleframework.xml.Root
+
+@Root(name = "F02_2014", strict = false)
+data class F01_2014(
+
+    @field:Attribute(name = "CATEGORY")
+    var category: String = "",
+
+    @field:Attribute(name = "FORM")
+    var form: String = "",
+
+    @field:Attribute(name = "LG")
+    var language: String = "",
+
+    @field:Element(name = "OBJECT_CONTRACT")
+    var contract: ObjectContract = ObjectContract(),
+
+    @field:Element(name = "CONTRACTING_BODY")
+    var contractingBody: ContractingBody = ContractingBody()
+
+)
